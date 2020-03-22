feb70a5c73e6923bc293b31e261661d1c537704e
Julius Villamayor <juliusvillamayor@gmail.com>
2020-03-21 18:45:16 -0700

    Add header

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import dynamic from 'next/dynamic'
 import Page from '../components/Page'
 import { ContextProvider } from '../utils/context'
+import Head from 'next/head'
 
 const MusicOverlay = dynamic(() => import('../components/MusicOverlay'), {
   ssr: false
@@ -9,6 +10,14 @@ const MusicOverlay = dynamic(() => import('../components/MusicOverlay'), {
 export default function Index() {
   return (
     <div>
+      <Head>
+        <link rel='icon' href='/favicon.ico' />
+        <meta
+          name='Julius Villamayor'
+          content='A Software Developer | A Graduate from the University of Victoria | BSc Computer Science and Psychology'
+        />
+        <title>Julius Villamayor</title>
+      </Head>
       <ContextProvider>
         <MusicOverlay />
         <Page idx={0} />
