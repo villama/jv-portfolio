97714e83aff6cd3ae15cbe72803c88a14a4d5e9a
Julius Villamayor <juliusvillamayor@gmail.com>
2020-07-11 11:10:24 -0700

    Fix transition bug, open links in new tab

--- components/Anchor.js
+++ components/Anchor.js
@@ -1,16 +1,16 @@
 export default function Anchor({ display, link }) {
   return (
-    <a className='anchor' href={link}>
+    <a className='anchor' href={link} target='_blank' rel='noopener noreferrer'>
       {display}
       <style jsx>{`
         .anchor {
           text-decoration: none;
           color: inherit;
+          transition: 200ms;
         }
         .anchor:hover {
           color: tomato;
           text-shadow: 0.5px 0px, 0.5px 0px;
-          transition: 200ms;
         }
       `}</style>
     </a>
