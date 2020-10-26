cffddc3fa4373849494f8020411fd62c197fc861
Julius Villamayor <juliusvillamayor@gmail.com>
2020-10-25 18:22:35 -0700

    Remove spaces

--- utils/composeClasses.tsx
+++ utils/composeClasses.tsx
@@ -1,11 +1,9 @@
 function composeClasses(...args: Array<string | null | undefined | false | 0>) {
   let output = ''
-
   args.forEach(arg => {
     if (!arg) return
     output += arg + ' '
   })
-
   return output.trim()
 }
 
