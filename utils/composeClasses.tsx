0f9591b4358ef7a10de41f16aab76ad49ae45b60
Julius Villamayor <juliusvillamayor@gmail.com>
2020-11-07 18:01:49 -0800

    Refactor

--- utils/composeClasses.tsx
+++ utils/composeClasses.tsx
@@ -1,8 +1,7 @@
 function composeClasses(...args: Array<string | null | undefined | false | 0>) {
   let output = ''
   args.forEach(arg => {
-    if (!arg) return
-    output += arg + ' '
+    if (arg) output += arg + ' '
   })
   return output.trim()
 }
